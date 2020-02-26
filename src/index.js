31f39d360c03416f48c23a7f941b9e76ca37c92f
web-mrAlderson <tima-999@inbox.ru>
2020-02-26 15:10:56 +0300

    feat: finction reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    let str = String(n);
+    str = str.substr(1);
+    let strReverse = str.split("").reverse().join("");
+    n = Number(strReverse);
+    return n;
+  }
+  else {
+    let str = String(n);
+    let strReverse = str.split("").reverse().join("");
+    n = Number(strReverse);
+    return n;
+  }
 }
